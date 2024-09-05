919e48bf70ea77b64603a5ea3300f3531a637aa0
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-05 13:51:01 -0400

    Add files via upload

---
+++ hartzell_TIY_5.3.py
@@ -0,0 +1,13 @@
+alien_color = ['green']
+print("\nIs alien green? yes/no?")
+
+if 'green' in alien_color:
+    print("You just earned 5 points")
+
+
+# Failing it
+alien_color = ['green']
+print("\nIs alien green? yes/no?")
+
+if 'yellow' in alien_color:
+    print("You just earned 5 points")
