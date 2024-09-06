13004bc48376fa5245c9f276db5b2682db32d38c
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-06 12:14:47 -0400

    Add files via upload

---
+++ hartzell_TIY_5.6.py
@@ -0,0 +1,97 @@
+# 5.6
+age = 1
+
+if age < 2:
+    print("This person's a baby")
+elif 2 <= age < 4:
+    print("This person's a toddler")
+elif 4 <= age < 13:
+    print("This person's a kid")
+elif 13 <= age < 20:
+    print("This person's a teenager")
+elif 20 <= age < 65:
+    print("This person's a adult")
+elif age < 65:
+    print(" This person's a senior")
+
+age = 2
+
+if age < 2:
+    print("This person's a baby")
+elif 2 <= age < 4:
+    print("This person's a toddler")
+elif 4 <= age < 13:
+    print("This person's a kid")
+elif 13 <= age < 20:
+    print("This person's a teenager")
+elif 20 <= age < 65:
+    print("This person's a adult")
+elif age < 65:
+    print(" This person's a senior")
+
+age = 4
+
+if age < 2:
+    print("This person's a baby")
+elif 2 <= age < 4:
+    print("This person's a toddler")
+elif 4 <= age < 13:
+    print("This person's a kid")
+elif 13 <= age < 20:
+    print("This person's a teenager")
+elif 20 <= age < 65:
+    print("This person's a adult")
+elif age < 65:
+    print(" This person's a senior")
+
+
+age = 13
+
+if age < 2:
+    print("This person's a baby")
+elif 2 <= age < 4:
+    print("This person's a toddler")
+elif 4 <= age < 13:
+    print("This person's a kid")
+elif 13 <= age < 20:
+    print("This person's a teenager")
+elif 20 <= age < 65:
+    print("This person's a adult")
+elif age < 65:
+    print(" This person's a senior")
+
+
+age = 21
+
+if age < 2:
+    print("This person's a baby")
+elif 2 <= age < 4:
+    print("This person's a toddler")
+elif 4 <= age < 13:
+    print("This person's a kid")
+elif 13 <= age < 20:
+    print("This person's a teenager")
+elif 20 <= age < 65:
+    print("This person's a adult")
+elif age < 65:
+    print(" This person's a senior")
+
+
+age = 67
+
+if age < 2:
+    print("This person's a baby")
+elif 2 <= age < 4:
+    print("This person's a toddler")
+elif 4 <= age < 13:
+    print("This person's a kid")
+elif 13 <= age < 20:
+    print("This person's a teenager")
+elif 20 <= age < 65:
+    print("This person's a adult")
+elif age > 65:
+    print(" This person's a senior")
+
+
+
+
