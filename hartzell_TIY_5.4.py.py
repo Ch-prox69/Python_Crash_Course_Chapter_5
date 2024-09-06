b2856c1b1e94d61de39292c65e425d39db628774
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-06 08:24:25 -0400

    Add files via upload

---
+++ hartzell_TIY_5.4.py.py
@@ -0,0 +1,17 @@
+# 5.4 guessing green
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien")
+else:
+    print("You just earned 10 point")
+    
+    
+    
+# guessing a wrong color    
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien")
+else:
+    print("You just earned 10 point")
