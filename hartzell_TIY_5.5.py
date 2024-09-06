0778d76ca028300a1ce952b06a0e8908555a9b73
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-06 11:51:51 -0400

    Add files via upload

---
+++ hartzell_TIY_5.5.py
@@ -0,0 +1,40 @@
+# 5.5 TIY Green
+print("green = 5 points \nyellow = 10 points \nred = 15 points")
+
+alien_color = 'green'
+if alien_color == 'green':
+    print("Player received 5 points")
+elif alien_color == 'yellow':
+    print("Player received 10 points")
+elif alien_color == 'red':
+    print("Player received 15 points")
+else:
+    print("Player has not guessed right alien color. Player receives no points. ")
+
+
+# Yellow
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("Player received 5 points")
+elif alien_color == 'yellow':
+    print("Player received 10 points")
+elif alien_color == 'red':
+    print("Player received 15 points")
+else:
+    print("Player has not guessed right alien color. Player receives no points. ")
+
+
+# Red
+alien_color = 'red'
+if alien_color == 'green':
+    print("Player received 5 points")
+elif alien_color == 'yellow':
+    print("Player received 10 points")
+elif alien_color == 'red':
+    print("Player received 15 points")
+else:
+    print("Player has not guessed right alien color. Player receives no points. ")
+
+
+
+
