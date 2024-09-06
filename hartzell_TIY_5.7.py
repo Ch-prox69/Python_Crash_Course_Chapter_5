d910d8786a41485128acba569cfed9746506db89
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-06 12:43:05 -0400

    Add files via upload

---
+++ hartzell_TIY_5.7.py
@@ -0,0 +1,13 @@
+# 5.7
+favorite_fruit = ['mango', 'kiwi', 'orange']
+
+if 'mango' in favorite_fruit:
+    print("I really like mango's")
+if 'kiwi' in favorite_fruit:
+    print("I really like kiwi")
+if 'orange' in favorite_fruit:
+    print("I really like orange")
+if 'lemon' in favorite_fruit:
+    print("Yuck, why would you eat a raw lemon")
+if 'pomegranate' in favorite_fruit:
+    print("I hate pomegranate")
